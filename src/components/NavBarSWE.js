25fa332e001b101aa4f3b9230835158372dc87ea
Manuel Morteo <manuelmort@Manuels-MacBook-Pro.local>
2022-10-26 14:32:12 -0700

    updated navbar for swe

--- src/components/NavBarSWE.js
+++ src/components/NavBarSWE.js
@@ -10,19 +10,23 @@ export default class NavBarSWE extends React.Component {
             <div class="container lg:mx-auto  bg-transparent">
             <nav class="flex justify-between bg-transparent pb-6 flex-wrap   lg:m-30 pt-6" >
                     <a  
-                        href="/" class="text-green-300 font-semibold text-3xl bg-transparent tracking-tight 
+                        href="/swe" class="text-green-300 font-semibold text-3xl bg-transparent tracking-tight 
                         hover:underline underline-offset-4 ">
-                        ECSJC</a>
+                        <img src="./swelogowhite.png" class="h-20 hover:bg-purple-300/30 rounded-lg p-2"/></a>
                 <div class="  lg:flex text-right  bg-transparent lg:items-center lg:w-auto ">
                     <div class="text-md bg-transparent lg:flex-grow">
-                        <Link to="Events" smooth={true} class="bg-transparent text-xl block mt-4 p-2 rounded-md md:inline-block lg:inline-block hover:text-violet-300 hover:bg-opacity-30 hover:bg-violet-300 lg:mt-0 text-indigo-100">
+                        <Link to="Events" smooth={true} class="bg-transparent text-xl block mt-4 p-2 rounded-md md:inline-block lg:inline-block hover:text-white hover:bg-opacity-30 hover:bg-violet-300 lg:mt-0 text-indigo-100">
                             Events
                         </Link>
        
                         <Link  to="Contact Us" spy={true} smooth={true}
-                            class="bg-transparent text-xl block mt-4 p-2 rounded-md lg:inline-block hover:text-purple-300 md:inline-block hover:bg-opacity-30 hover:bg-purple-300 lg:mt-0 text-indigo-100">
+                            class="bg-transparent text-xl block mt-4 p-2 rounded-md lg:inline-block hover:text-white md:inline-block hover:bg-opacity-30 hover:bg-purple-300 lg:mt-0 text-indigo-100">
                             Contact Us
                         </Link>
+                        <a  href="/" spy={true} smooth={true}
+                            class="bg-transparent text-xl block mt-4 p-2 rounded-md lg:inline-block hover:text-white md:inline-block hover:bg-opacity-30 hover:bg-purple-300 lg:mt-0 text-indigo-100">
+                            ECSJC
+                        </a>
 
                         
                     </div>
